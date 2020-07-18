8e7219375b89a23c5be9a72663eb1349c7a8e4d9
01float <vagrant@vvv>
2020-07-18 22:33:26 +0000

    widgets and sidebars

--- public_html/wp-content/themes/landing/header.php
+++ public_html/wp-content/themes/landing/header.php
@@ -6,6 +6,6 @@
 		</title>
 		<!-- Bootstrap core CSS pliki pobierane z CDN -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link href="style.css" rel="stylesheet">
+		href="<?php echo get_bloginfo('template_directory'); ?>"/style.css"
 	</head>
 	<body>
